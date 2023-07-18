724334c91e155f536737ce55b2f5b00f4d21ad27
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 15:18:41 +0530

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,30 @@
+/* Nested if else Statement */
+class Nested
+{
+    public static void main(String[] args) {
+
+        int a=10,b=20,c=30;
+        if(a>b)
+        {
+            if(a>c)
+            {
+                System.out.println(a);
+            }
+            else
+            {
+                System.out.println(c);
+            }
+        }
+        else
+        {
+            if(b>c)
+            {
+                System.out.println(b);
+            }
+            else
+            {
+                System.out.println(c);
+            }
+        }
+    }
+}
